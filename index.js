2044a149ffc56acfae08de32b4481c40d0c1fb98
Md Morsed Alam <mdmorsedalam68@gmail.com>
2023-08-16 09:06:37 +0600

    some changes

--- index.js
+++ index.js
@@ -2,17 +2,18 @@
 const alhamdoCount=document.getElementById("alhamdo-count");
 const alhamdoString=alhamdoCount.innerText;
 let alhamdoValue=parseFloat(alhamdoString);
+let alhamdoResult=1;
 document.querySelector("#alhamdo-inc").addEventListener("click",function(){
-if(alhamdoValue<=33){
-    document.getElementById("alhamdo-count").innerText=alhamdoValue++;
+if(alhamdoResult<=33){
+    document.getElementById("alhamdo-count").innerText=alhamdoResult++;
 }else{
     return alert("You Have Finished 33 Please Go To Next");
 }   
 })
 // ---------Alhamdolillah Decriment-----
 document.querySelector("#alhamdo-dec").addEventListener("click",function(){
-    if(alhamdoValue>=0){
-        document.getElementById("alhamdo-count").innerText=alhamdoValue--;
+    if(alhamdoResult>=0){
+        document.getElementById("alhamdo-count").innerText=alhamdoResult--;
     }else{
         return alert("Your Count Is Under 0 Please The Incriment Button");
     }
@@ -22,17 +23,18 @@ document.querySelector("#alhamdo-dec").addEventListener("click",function(){
 const allahoCount=document.getElementById("allaho-count");
 const allahoString=allahoCount.innerText;
 let allahoValue=parseFloat(allahoString);
+let allahoResult=1;
 document.querySelector("#allaho-inc").addEventListener("click",function(){
-if(allahoValue<=33){
-    document.getElementById("allaho-count").innerText=allahoValue++;
+if(allahoResult<=33){
+    document.getElementById("allaho-count").innerText=allahoResult++;
 }else{
     return alert("You Have Finished 33 Please Go To Next");
 }   
 })
     // ---------Allaho Akbar Decriment-----
     document.querySelector("#allaho-dec").addEventListener("click",function(){
-        if(allahoValue>=0){
-            document.getElementById("allaho-count").innerText=allahoValue--;
+        if(allahoResult>=0){
+            document.getElementById("allaho-count").innerText=allahoResult--;
         }else{
             return alert("Your Count Is Under 0 Please The Incriment Button");
         }
@@ -42,17 +44,19 @@ if(allahoValue<=33){
 const subahanCount=document.getElementById("subahan-count");
 const subahanString=subahanCount.innerText;
 let subahanValue=parseFloat(subahanString);
+let subahanResult=1;
 document.querySelector("#subahan-inc").addEventListener("click",function(){
-if(subahanValue<=34){
-    document.getElementById("subahan-count").innerText=subahanValue++;
+if(subahanResult<=34){
+    
+    document.getElementById("subahan-count").innerText=subahanResult++;;
 }else{
     return alert("You Have Finished 34 Please Go Again");
 }   
 })
     // ---------Allaho Akbar Decriment-----
     document.querySelector("#subahan-dec").addEventListener("click",function(){
-        if(subahanValue>=0){
-            document.getElementById("subahan-count").innerText=subahanValue--;
+        if(subahanResult>=0){
+            document.getElementById("subahan-count").innerText=subahanResult--;
         }else{
             return alert("Your Count Is Under 0 Please The Incriment Button");
         }
